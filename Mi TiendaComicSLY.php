53b9452386fe8e2ab9d4a20fe37ddadddff5c21f
sylvetser <savestine@gmail.com>
2025-01-21 09:55:55 +0100

    comic

---
+++ Mi TiendaComicSLY.php
@@ -0,0 +1,74 @@
+<?php
+// Definición del inventario de cómics
+$inventario = [
+    'suspense_terror' => [
+        ['titulo' => 'The Long Halloween', 'editorial' => 'DC', 'autor' => 'Tim Sale', 'idioma' => 'Inglés', 'precio' => 20, 'stock' => 10],
+        ['titulo' => 'Uzumaki', 'editorial' => 'Planeta', 'autor' => 'Junji Ito', 'idioma' => 'Japonés', 'precio' => 25, 'stock' => 15],
+        ['titulo' => 'Tomie', 'editorial' => 'Planeta', 'autor' => 'Junji Ito', 'idioma' => 'Japonés', 'precio' => 25, 'stock' => 20],
+    ],
+    'accion' => [
+        ['titulo' => 'Berserk Deluxe Edition 1', 'editorial' => 'Dark Horse', 'autor' => 'Kentaro Miura', 'idioma' => 'Japonés', 'precio' => 30, 'stock' => 12],
+    ],
+    // Puedes agregar más categorías y cómics según sea necesario
+];
+
+// Main
+// Antes del descuento
+mostrarComicsEnTabla();
+mostrarValorAlmacen();
+aplicarDescuentoManga();
+// Mostramos el inventario actualizado
+mostrarComicsEnTabla();
+mostrarValorAlmacen();
+
+function mostrarComicsEnTabla()
+{
+    global $inventario;
+    echo '<br>';
+    echo '<table border="1">';
+    echo '<tr><th>Categoría</th><th>Título</th><th>Editorial</th><th>Autor</th><th>Idioma</th><th>Precio</th><th>Stock</th></tr>';
+
+    foreach ($inventario as $categoria => $comics) {
+        foreach ($comics as $comic) {
+            echo '<tr>';
+            echo "<td>$categoria</td>";
+            echo "<td>{$comic['titulo']}</td>";
+            echo "<td>{$comic['editorial']}</td>";
+            echo "<td>{$comic['autor']}</td>";
+            echo "<td>{$comic['idioma']}</td>";
+            echo "<td>{$comic['precio']}</td>";
+            echo "<td>{$comic['stock']}</td>";
+            echo '</tr>';
+        }
+    }
+
+    echo '</table>';
+}
+
+function mostrarValorAlmacen()
+{
+    global $inventario;
+    $valorTotal = 0; // Inicializamos la variable
+
+    foreach ($inventario as $categoria => $comics) {
+        foreach ($comics as $comic) {
+            $cal = $comic['precio'] * $comic['stock'];
+            $valorTotal += $cal;
+        }
+    }
+    echo "El valor total del almacén es: $valorTotal";
+}
+
+function aplicarDescuentoManga()
+{
+    global $inventario;
+
+    foreach ($inventario as $categoria => &$comics) {
+        foreach ($comics as &$comic) {
+            if ($comic['idioma'] == 'Japonés') {
+                $comic['precio'] *= 0.7; // Aplicar descuento del 30%
+            }
+        }
+    }
+}
+?>
